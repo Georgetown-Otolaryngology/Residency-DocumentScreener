07990561167a9d6e37dad31e950f8230a9e59f2b
chrislearns <77995541+chrislearns@users.noreply.github.com>
2023-09-08 07:11:21 -0400

    Initial Commit

---
+++ DocumentScreener/DocumentScreenerApp.swift
@@ -0,0 +1,17 @@
+//
+//  DocumentScreenerApp.swift
+//  DocumentScreener
+//
+//  Created by Christopher Guirguis on 9/8/23.
+//
+
+import SwiftUI
+
+@main
+struct DocumentScreenerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
